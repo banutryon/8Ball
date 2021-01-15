12c916c4a3aa1035ad77904dac17ea5bc269c6b7
ajigherighe <akpoigherighe@gmail.com>
2021-01-14 20:22:12 -0500

    added update and delete routes to controller

--- controllers/8ball_controller.js
+++ controllers/8ball_controller.js
@@ -4,11 +4,37 @@ const EightBall = require("../models/8ball_model.js");
 
 // ROUTES
 eightBall.get("/", (req, res) => {
-    res.send("index");
+    EightBall.find({}, (err, foundAnswers) => {
+        res.json(foundAnswers);
+    });
 });
 
 eightBall.post("/", (req, res) => {
     EightBall.create(req.body, (err, createdAnswer) => {
+        EightBall.find({}, (err, foundAnswers) => {
+            res.json(foundAnswers);
+        });
+    });
+});
+
+eightBall.put("/:id", (req, res) => {
+    EightBall.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedAnswer) => {
+            if (err) {
+                res.send(err);
+            } else {
+                EightBall.find({}, (err, foundAnswers) => {
+                    res.json(foundAnswers);
+                });
+            }
+        });
+});
+
+eightBall.delete("/:id", (req, res) => {
+    EightBall.findByIdAndRemove(req.params.id, (err, deletedAnswer) => {
         EightBall.find({}, (err, foundAnswers) => {
             res.json(foundAnswers);
         });
