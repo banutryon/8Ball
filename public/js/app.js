7de3249d0315e748c25844312633fb7902e6456d
banu tryon <banutryon@banus-mbp.lan>
2021-01-17 22:03:49 -0800

    css mobile v27

--- public/js/app.js
+++ public/js/app.js
@@ -103,7 +103,7 @@ class App extends React.Component {
         <summary><img className="ballCreate"
         src="ball.png" alt="Create Ball" /></summary>
         <div className="formBorder">
-        <p>We wanted to share a fun app that is designed to help with stress and put a smile on your face. Simply ask the 8 ball a question and click the ask button.In this app you can add new gifs by simply adding a name and url to the creat button. This button is hidden inside the 8 ball logo. </p>
+        <p>We wanted to share a fun app that is designed to help with stress and put a smile on your face. Simply ask the 8 Ball a question and click the ask button. By selecting the image inside of the 8 Ball you can also edit or delete any of the giphys.  </p>
 
         <h3>You can also add a new Gif to 8 Ball in this form</h3>
 
